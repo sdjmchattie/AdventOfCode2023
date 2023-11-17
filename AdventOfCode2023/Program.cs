96ea25fa1045f99570d74b3b1633fb05c8f36d48
Stuart McHattie <stuart.mchattie@tessella.com>
2023-11-17 18:49:14 +0000

    Prepare the pattern for puzzles

---
+++ AdventOfCode2023/Program.cs
@@ -0,0 +1,6 @@
+﻿using AdventOfCode.Y2023;
+
+var solution = new Day01();
+
+Console.WriteLine(solution.Part1(""));
+Console.WriteLine(solution.Part2(""));
---
+++ AdventOfCode2023/Puzzles/Day01/Solution.cs
@@ -0,0 +1,13 @@
+namespace AdventOfCode.Y2023;
+
+class Day01 {
+    public object Part1(string input)
+    {
+        return "Hello";
+    }
+
+    public object Part2(string input)
+    {
+        return "Hello World!";
+    }
+}
