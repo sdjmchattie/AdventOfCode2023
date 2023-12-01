6a316ab3475b704709d97c5e980e1309f4bf7efd
Stuart McHattie <stuart.mchattie@tessella.com>
2023-12-01 10:49:42 +0000

    Add solution for Day 1 Part 2

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -2,5 +2,5 @@
 
 var solution = new Day01();
 
-Console.WriteLine("Part 1:" + solution.Part1());
-Console.WriteLine("Part 2:" + solution.Part2());
+Console.WriteLine("Part 1:  " + solution.Part1());
+Console.WriteLine("Part 2:  " + solution.Part2());
--- AdventOfCode2023/Puzzles/Day01/Solution.cs
+++ AdventOfCode2023/Puzzles/Day01/Solution.cs
@@ -6,32 +6,58 @@ using Microsoft.VisualBasic;
 namespace AdventOfCode.Y2023;
 
 class Day01 {
-    List<int>? calibrationValues;
-
-    private void ParseInput() {
-        if (calibrationValues is not null) {
-            return;
-        }
-
+    private IEnumerable<int> ParseInputPart1() {
         var inputLines = File.ReadAllLines("Puzzles/Day01/input.txt");
         var rxLeft = new Regex(@"^\D*(\d)");
         var rxRight = new Regex(@"(\d)\D*$");
-        calibrationValues = inputLines
+        return inputLines
             .Select<string, IEnumerable<Match>>(line => [rxLeft.Match(line), rxRight.Match(line)])
             .Select(matches => matches.First().Groups[1].ToString() + matches.Last().Groups[1].ToString())
-            .Select(int.Parse)
-            .ToList();
+            .Select(int.Parse);
     }
 
     public string Part1()
     {
-        ParseInput();
-        return calibrationValues!.Sum().ToString();
+        var input = ParseInputPart1();
+        return input.Sum().ToString();
+    }
+
+    private string NumericWordsToDigits(string numericWords) {
+        var output = "";
+        var regex = new Regex(@"(?=(\d|one|two|three|four|five|six|seven|eight|nine))");
+        var matches = regex.Matches(numericWords);
+        var sortedMatches = matches.OrderBy(e => e.Index);
+
+        foreach (Match match in sortedMatches) {
+            output += match.Groups[1].ToString();
+        }
+
+        return output
+            .Replace("one", "1")
+            .Replace("two", "2")
+            .Replace("three", "3")
+            .Replace("four", "4")
+            .Replace("five", "5")
+            .Replace("six", "6")
+            .Replace("seven", "7")
+            .Replace("eight", "8")
+            .Replace("nine", "9");
+    }
+
+    private IEnumerable<int> ParseInputPart2() {
+        var inputLines = File.ReadAllLines("Puzzles/Day01/input.txt");
+        var rxLeft = new Regex(@"^\D*(\d)");
+        var rxRight = new Regex(@"(\d)\D*$");
+        return inputLines
+            .Select(NumericWordsToDigits)
+            .Select<string, IEnumerable<Match>>(line => [rxLeft.Match(line), rxRight.Match(line)])
+            .Select(matches => matches.First().Groups[1].ToString() + matches.Last().Groups[1].ToString())
+            .Select(int.Parse);
     }
 
     public string Part2()
     {
-        ParseInput();
-        return "Hello World!";
+        var input = ParseInputPart2();
+        return input.Sum().ToString();
     }
 }
