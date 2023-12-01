3783985b771c730e8787dcdd92b1d9192fd0d5a3
Stuart McHattie <stuart.mchattie@tessella.com>
2023-12-01 09:52:25 +0000

    Complete Day 1 Part 1

--- AdventOfCode2023/Puzzles/Day01/Solution.cs
+++ AdventOfCode2023/Puzzles/Day01/Solution.cs
@@ -1,13 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using AdventOfCode.Utils;
+using Microsoft.VisualBasic;
+
 namespace AdventOfCode.Y2023;
 
 class Day01 {
-    public object Part1(string input)
-    {
-        return "Hello";
+    List<int>? calibrationValues;
+
+    private void ParseInput() {
+        if (calibrationValues is not null) {
+            return;
+        }
+
+        var inputLines = File.ReadAllLines("Puzzles/Day01/input.txt");
+        var rxLeft = new Regex(@"^\D*(\d)");
+        var rxRight = new Regex(@"(\d)\D*$");
+        calibrationValues = inputLines
+            .Select<string, IEnumerable<Match>>(line => [rxLeft.Match(line), rxRight.Match(line)])
+            .Select(matches => matches.First().Groups[1].ToString() + matches.Last().Groups[1].ToString())
+            .Select(int.Parse)
+            .ToList();
     }
 
-    public object Part2(string input)
+    public string Part1()
     {
+        ParseInput();
+        return calibrationValues!.Sum().ToString();
+    }
+
+    public string Part2()
+    {
+        ParseInput();
         return "Hello World!";
     }
 }
