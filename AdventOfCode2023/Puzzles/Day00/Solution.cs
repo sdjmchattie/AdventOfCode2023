3b7783b41eef2a9636ca9ad82b0055b1012afcd2
Stuart McHattie <stuart.mchattie@gmail.com>
2023-12-03 18:21:57 +0000

    Add template for solutions

---
+++ AdventOfCode2023/Puzzles/Day00/Solution.cs
@@ -0,0 +1,13 @@
+namespace AdventOfCode.Y2023;
+
+class Day00 {
+    public object Part1(string input)
+    {
+        return "Part 1 Solution";
+    }
+
+    public object Part2(string input)
+    {
+        return "Part 2 Solution";
+    }
+}
