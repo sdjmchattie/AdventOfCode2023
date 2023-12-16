25610e4d97fec71049cc310a4542a2b25a1e91e7
Stuart McHattie <stuart.mchattie@gmail.com>
2023-12-16 19:42:24 +0000

    Prepare files for day 17

---
+++ AdventOfCode2023/Y2023/Day17/Solution.cs
@@ -0,0 +1,19 @@
+namespace AdventOfCode.Y2023;
+
+class Day17 {
+    private string[]? inputContents;
+    private string[] InputContents =>
+        inputContents ??= File.ReadAllLines($"Y2023/{GetType().Name}/input.txt");
+
+    public object Part1()
+    {
+        var input = InputContents;
+        return "Part 1 Solution";
+    }
+
+    public object Part2()
+    {
+        var input = InputContents;
+        return "Part 2 Solution";
+    }
+}
