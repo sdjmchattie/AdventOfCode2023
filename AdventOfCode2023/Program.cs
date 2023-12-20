4c440a6ca67a88350d780bd6b23a542439b2d33d
Stuart McHattie <stuart.mchattie@gmail.com>
2023-12-20 22:53:09 +0000

    Prepare for day 20

--- AdventOfCode2023/Program.cs
+++ AdventOfCode2023/Program.cs
@@ -1,6 +1,6 @@
 ﻿using AdventOfCode.Y2023;
 
-var solution = new Day19();
+var solution = new Day20();
 
 Console.WriteLine("Part 1:  " + solution.Part1());
 Console.WriteLine("Part 2:  " + solution.Part2());
